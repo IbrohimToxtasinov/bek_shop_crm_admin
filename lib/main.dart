e823f80029857fa7abaebe8c3931a43b8239ad0c
IbrohimToxtasinov <toxtasinovibrohim976@gmail.com>
2025-03-27 16:51:49 +0500

    added some changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:bek_shop/firebase_options.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
@@ -13,14 +14,20 @@ void main() async {
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1aG1mcmFjZWpuYXRwam1rc29hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1NzcxNjIsImV4cCI6MjA1NjE1MzE2Mn0.QxOvFZrYtMkRoyDpCk4FzkoEOgIGe7QChse2HkttYUc",
   );
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(
-    EasyLocalization(
-      startLocale: const Locale('uz', 'UZ'),
-      supportedLocales: const [Locale('uz', 'UZ'), Locale('ru', 'RU'), Locale('en', 'EN')],
-      path: 'assets/translations',
-      fallbackLocale: const Locale('uz', 'UZ'),
-      saveLocale: true,
-      child: App(),
-    ),
-  );
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(
+      EasyLocalization(
+        startLocale: const Locale('uz', 'UZ'),
+        supportedLocales: const [Locale('uz', 'UZ'), Locale('ru', 'RU'), Locale('en', 'EN')],
+        path: 'assets/translations',
+        fallbackLocale: const Locale('uz', 'UZ'),
+        saveLocale: true,
+        child: App(),
+      ),
+    );
+  });
 }
